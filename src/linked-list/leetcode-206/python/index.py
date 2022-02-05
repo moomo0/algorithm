6477fb633cf388cc1b98c983cffca0cfa2369f67
WeiDaiATM <daiwei@tju.edu.cn>
2022-02-05 14:13:42 +0800

    linked-list 206

---
+++ src/linked-list/leetcode-206/python/index.py
@@ -0,0 +1,14 @@
+class Solution(object):
+	def reverseList(self, head):
+		"""
+		:type head: ListNode
+		:rtype: ListNode
+		"""
+		pre = None
+		cur = head
+		while cur:
+			tmp = cur.next
+			cur.next = pre
+			pre = cur
+			cur = tmp
+		return pre	
