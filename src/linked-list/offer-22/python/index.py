2f18c7546841b5552131e844d4b1ab44d9c61234
WeiDaiATM <daiwei@tju.edu.cn>
2022-02-18 15:27:09 +0800

    O22 python

--- src/linked-list/offer-22/python/index.py
+++ src/linked-list/offer-22/python/index.py
@@ -1,1 +1,20 @@
 
+class Solution(object):
+    def getKthFromEnd(self, head, k):
+        """
+        :type head: ListNode
+        :type k: int
+        :rtype: ListNode
+        """
+        counter = 0
+        temp = head
+        while(temp):
+            temp = temp.next
+            counter += 1
+        if counter < k:
+            return None
+        else:
+            for i in range(counter - k):
+                head = head.next
+
+            return head
